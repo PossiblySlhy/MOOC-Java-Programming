d31d2b16c05c5b59cbc9c4bf7f9a19f99676dcbf
Blake <db.phi@proton.me>
2026-07-27 10:15:30 -0500

    Added main test to I.5.1

--- Part 5/part05-Part05_01.OneMinute/src/main/java/Program.java
+++ Part 5/part05-Part05_01.OneMinute/src/main/java/Program.java
@@ -5,7 +5,17 @@ public class Program {
 
     public static void main(String[] args) {
         // You can test your program here
+        Timer timer = new Timer();
 
+        while (true) {
+            System.out.println(timer.toString());
+            timer.advance();
 
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+
+            }
+        }
     }
 }
