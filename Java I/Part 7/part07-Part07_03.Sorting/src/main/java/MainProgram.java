56b04628a465134c9c0cc668f31c1dbb772b51e3
Blake <db.phi@proton.me>
2026-08-11 15:33:52 -0500

    Completed I.7.3-4 exercises

--- Java I/Part 7/part07-Part07_03.Sorting/src/main/java/MainProgram.java
+++ Java I/Part 7/part07-Part07_03.Sorting/src/main/java/MainProgram.java
@@ -1,9 +1,68 @@
+import java.util.Arrays;
 
 public class MainProgram {
 
     public static void main(String[] args) {
         // write your test code here
-        int[] array = {3, 1, 5, 99, 3, 12};
+        int[] array = { 3, 1, 5, 99, 3, 12 };
+        System.out.println(smallest(array));
+        System.out.println(indexOfSmallest(array));
+        System.out.println(indexOfSmallestFrom(array, 2));
+
+        MainProgram.sort(array);
     }
 
+    public static int smallest(int[] array) {
+        int smallest = array[0];
+
+        for (int num : array) {
+            if (num < smallest) {
+                smallest = num;
+            }
+        }
+
+        return smallest;
+    }
+
+    public static int indexOfSmallest(int[] array) {
+        int smallest = array[0];
+        int smallestIndex = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < smallest) {
+                smallest = array[i];
+                smallestIndex = i;
+            }
+        }
+
+        return smallestIndex;
+    }
+
+    public static int indexOfSmallestFrom(int[] array, int start) {
+        int smallest = array[start];
+        int smallestIndex = start;
+
+        for (int i = start; i < array.length; i++) {
+            if (array[i] < smallest) {
+                smallest = array[i];
+                smallestIndex = i;
+            }
+        }
+
+        return smallestIndex;
+    }
+
+    public static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
+    public static void sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int smallIndex = indexOfSmallestFrom(array, i);
+            swap(array, i, smallIndex);
+            System.out.println(Arrays.toString(array));
+        }
+    }
 }
--- Java I/Part 7/part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
+++ Java I/Part 7/part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
@@ -1,10 +1,47 @@
-
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
         // insert test code here
-        int[] array = {3, 1, 5, 99, 3, 12};
+        int[] intArray = { 3, 1, 5, 99, 3, 12 };
+        String[] stringArray = { "b", "d", "z", "a", "r" };
+        ArrayList<Integer> intList = new ArrayList<>();
+        ArrayList<String> stringList = new ArrayList<>();
+
+        for (int num : intArray) {
+            intList.add(num);
+        }
+
+        for (String word : stringArray) {
+            stringList.add(word);
+        }
+
+        sort(intArray);
+        sort(stringArray);
+        sortIntegers(intList);
+        sortStrings(stringList);
+
+        System.out.println(Arrays.toString(intArray));
+        System.out.println(Arrays.toString(stringArray));
+        System.out.println(intList);
+        System.out.println(stringList);
     }
 
+    public static void sort(int[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void sort(String[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void sortIntegers(ArrayList<Integer> ints) {
+        ints.sort(null);
+    }
+
+    public static void sortStrings(ArrayList<String> strings) {
+        strings.sort(null);
+    }
 }
